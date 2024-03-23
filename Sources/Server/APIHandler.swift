7c8ac23e4752cd0ac2eb57f75ef2ec43d4528976
zunda <47569369+zunda-pixel@users.noreply.github.com>
2024-03-23 19:45:57 +0900

    Add location api (#10)
    * add tripadvisor-kit
    
    * move Use
    
    * add location api
    
    * move file
    
    * remove Fluent

--- Sources/Server/APIHandler.swift
+++ Sources/Server/APIHandler.swift
@@ -2,8 +2,17 @@ import Vapor
 
 actor APIHandler: APIProtocol {
   let app: Application
+  let tripadvisorApiKey: String
+  var tripadvisorRefererURL: URL? {
+    #if DEBUG
+      return URL(string: "https://api.niabis.com")!
+    #else
+      return nil
+    #endif
+  }
 
   init(app: Application) {
     self.app = app
+    self.tripadvisorApiKey = Environment.get("TRIPADVISOR_API_KEY")!
   }
 }
