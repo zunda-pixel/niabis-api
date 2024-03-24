dd3adeb552005e1835361a486a4f669a056fe28d
zunda <47569369+zunda-pixel@users.noreply.github.com>
2024-03-24 21:26:04 +0900

    remove post, delete

--- Sources/Server/API/UserAPI.swift
+++ Sources/Server/API/UserAPI.swift
@@ -14,31 +14,6 @@ extension APIHandler {
     return .ok(.init(body: .json(user.componentUser)))
   }
 
-  func postUser(_ input: Operations.postUser.Input) async throws -> Operations.postUser.Output {
-    guard case .json(let user) = input.body else { return .badRequest(.init()) }
-    try await user.dbUser.create(on: app.db)
-    return .ok(.init(body: .json(user)))
-  }
-
-  func deleteUserByID(_ input: Operations.deleteUserByID.Input) async throws
-    -> Operations.deleteUserByID.Output
-  {
-    guard let userID = UUID(uuidString: input.query.userID) else { return .badRequest(.init()) }
-
-    let userCount = try await User.query(on: app.db)
-      .filter(\.$id, .equal, userID).limit(1).count()
-
-    guard userCount > 0 else {
-      return .notFound(.init())
-    }
-
-    try await User.query(on: app.db)
-      .filter(\.$id, .equal, userID)
-      .delete()
-
-    return .noContent(.init())
-  }
-
   func updateUserByID(_ input: Operations.updateUserByID.Input) async throws
     -> Operations.updateUserByID.Output
   {
