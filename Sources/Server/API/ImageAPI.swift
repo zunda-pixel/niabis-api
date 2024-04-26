f35c559792c234bbb6324ad603018b4c1aff2227
zunda <47569369+zunda-pixel@users.noreply.github.com>
2024-04-27 03:28:55 +0900

    refactor

--- Sources/Server/API/ImageAPI.swift
+++ Sources/Server/API/ImageAPI.swift
@@ -11,19 +11,17 @@ extension APIHandler {
       accountId: cloudflareAccountId
     )
 
-    let uploadedImage: Image
-
-    if case .image__ast_(let body) = input.body {
-      let imageData = try await Data(collecting: body, upTo: .max)
-      do {
-        uploadedImage = try await clinet.upload(imageData: imageData)
-      } catch RequestError.invalidContentType {
-        throw Abort(.internalServerError, reason: "Inavlid Content-Type. image must have image/jpeg, image/png, image/webp, image/gif or image/svg+xml content-type")
-      }
-    } else {
+    guard case .image__ast_(let body) = input.body else {
       throw Abort(.badRequest, reason: "Requires Image data or Image url")
     }
-
+    let imageData = try await Data(collecting: body, upTo: .max)
+    let uploadedImage: Image
+    do {
+      uploadedImage = try await clinet.upload(imageData: imageData)
+    } catch RequestError.invalidContentType {
+      throw Abort(.internalServerError, reason: "Inavlid Content-Type. image must have image/jpeg, image/png, image/webp, image/gif or image/svg+xml content-type")
+    }
+    
     guard let imageURL = uploadedImage.variants.first else {
       throw Abort(.internalServerError, reason: "Not found Image")
     }
