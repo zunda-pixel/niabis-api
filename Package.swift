5e6f0fe97469c7f7c6dd1fce3df309f7981c0596
zunda <zunda.dev@gmail.com>
2024-06-13 23:44:43 +0900

    update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,39 +2,6 @@
 
 import PackageDescription
 
-var dependencies: [Package.Dependency] = [
-  .package(url: "https://github.com/apple/swift-openapi-generator", from: "1.2.1"),
-  .package(url: "https://github.com/swift-server/swift-openapi-async-http-client", from: "1.0.0"),
-  .package(url: "https://github.com/swift-server/swift-openapi-vapor", from: "1.0.1"),
-  .package(url: "https://github.com/vapor/fluent-postgres-driver", from: "2.8.0"),
-  .package(url: "https://github.com/vapor/fluent", from: "4.9.0"),
-  //.package(url: "https://github.com/apple/swift-format", from: "510.1.0"),
-  .package(url: "https://github.com/apple/swift-metrics", from: "2.4.1"),
-  .package(url: "https://github.com/swift-server/swift-prometheus", from: "2.0.0"),
-  .package(url: "https://github.com/zunda-pixel/tripadvisor-swift", from: "0.2.0"),
-  .package(url: "https://github.com/vapor/jwt", from: "5.0.0-beta.4"), // TODO
-  .package(url: "https://github.com/supabase/supabase-swift", from: "2.8.0"),
-  .package(url: "https://github.com/zunda-pixel/cloudflare-swift", from: "0.1.1"),
-]
-
-#if !os(macOS)
-package.dependencies.append(.package(url: "https://github.com/apple/swift-testing", from: "0.10.1"))
-#endif
-
-var testTarget = Target.testTarget(
-  name: "AppTests",
-  dependencies: [
-    .target(name: "App"),
-  ],
-  resources: [
-    .process("Resources"),
-  ]
-)
-
-#if !os(macOS)
-testTarget.dependencies.append(.product(name: "Testing", package: "swift-testing"))
-#endif
-
 let package = Package(
   name: "niabis-api",
   platforms: [
@@ -44,7 +11,21 @@ let package = Package(
     .watchOS(.v6),
     .visionOS(.v1)
   ],
-  dependencies: dependencies,
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-openapi-generator", from: "1.2.1"),
+    .package(url: "https://github.com/swift-server/swift-openapi-async-http-client", from: "1.0.0"),
+    .package(url: "https://github.com/swift-server/swift-openapi-vapor", from: "1.0.1"),
+    .package(url: "https://github.com/vapor/fluent-postgres-driver", from: "2.8.0"),
+    .package(url: "https://github.com/vapor/fluent", from: "4.9.0"),
+    //.package(url: "https://github.com/apple/swift-format", from: "510.1.0"),
+    .package(url: "https://github.com/apple/swift-metrics", from: "2.4.1"),
+    .package(url: "https://github.com/swift-server/swift-prometheus", from: "2.0.0"),
+    .package(url: "https://github.com/zunda-pixel/tripadvisor-swift", from: "0.2.0"),
+    .package(url: "https://github.com/vapor/jwt", from: "5.0.0-beta.4"), // TODO
+    .package(url: "https://github.com/supabase/supabase-swift", from: "2.8.0"),
+    .package(url: "https://github.com/zunda-pixel/cloudflare-swift", from: "0.1.1"),
+    .package(url: "https://github.com/apple/swift-testing", from: "0.9.0"),
+  ],
   targets: [
     .executableTarget(
       name: "App",
@@ -65,6 +46,15 @@ let package = Package(
         .plugin(name: "OpenAPIGenerator", package: "swift-openapi-generator")
       ]
     ),
-    testTarget,
+    .testTarget(
+      name: "AppTests",
+      dependencies: [
+        .target(name: "App"),
+        .product(name: "Testing", package: "swift-testing"),
+      ],
+      resources: [
+        .process("Resources"),
+      ]
+    ),
   ]
 )
